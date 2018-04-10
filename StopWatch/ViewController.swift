4835f23ce962b4315e89a32e0e4ae90fab817823
정순우 <soonwoo2714@gmail.com>
2018-04-10 17:01:51 +0900

    StopWatch_First

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timeLabel: UILabel!
+    var count = 0
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func timerStart(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in
+            self.updateTime()
+    })
     }
-
-
+    
+    
+    @IBAction func timerStop(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    
+    @IBAction func timerReset(_ sender: Any) {
+        myTimer.invalidate()
+        timeLabel.text = String(format: "00:00:00")
+        count = 0
+        
+    }
+    
+    func updateTime() {
+        count = count + 1
+        let min = count / 60 / 100
+        let sec = (count - (min * 60 * 100)) / 100
+        let msec = count - (min * 60 * 100) - (sec * 100)
+        timeLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
+    }
+    
 }
+
 
